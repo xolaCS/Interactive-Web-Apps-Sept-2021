12d922141e9039ca739de03150f0f0fabb3deefd
xdossantos <xola.dossantos@xdode.com>
2021-09-22 18:05:58 +0200

    fixes

---
+++ static/js/logic.js
@@ -0,0 +1,35 @@
+  
+
+        let currentPassword = ''; 
+        let age = 24; 
+
+        console.log(age); 
+        let x = null ; 
+        const y = undefined ; 
+        console.log(y); 
+
+
+        // document.getElementById("myButton").onclick = function() {
+        //     document.getElementById("myText").innerHTML = updatedOutput; 
+        // }
+
+        // function myFunction() {
+           
+        // }
+
+        const myFunction = () => {
+            const updatedOutput = "Hello World";
+
+            console.log(updatedOutput); 
+            document.getElementById("myText").innerHTML = updatedOutput; 
+        }
+
+
+
+        // class Pet {
+        //     constructor(uniqueName){
+        //         this.Name = uniqueName; 
+        //     }
+        // }
+
+        // let myPet = new Pet("Bobby");
